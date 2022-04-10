8f1cdf73a1883ffe9a8bebdb29343ae6256f6073
seansie0830 <seansie0830@gmail.com>
2022-04-10 16:34:32 +0800

    a040

--- c++/a040.cpp
+++ c++/a040.cpp
@@ -1,22 +1,34 @@
+// AC (58ms, 348KB)
 #include<bits/stdc++.h>
 using namespace std;
 int power(int,int);
-bool ckeck(string,int);
+bool check(string,int);
+bool isnum=false;
 int main(){
     int a,b,i,j;
     cin>>a>>b;
     stringstream stream;
     string out_str;
     for(i=a;i<=b;i++){
+        stream.str("");
+        stream.clear();
         stream<<i;
         stream>>out_str;
-        if(ckeck(out_str,i)) cout<<i<<endl;
+        if(check(out_str,i)) cout<<i<<" ";
     }
+    if(isnum==false) cout<<"none"<<endl;
+    else cout<<"\b";
     return 0;
 }
-bool ckeck(string in_str,int in_int){
+bool check(string in_str,int in_int){
+    bool out;
     int power_sum=0,i;
     for(i=0;i<in_str.length();i++){
-        power_sum=power_sum+pow((float)in_str[i],4.0);
+        power_sum=power_sum+pow((float)in_str[i]-48,in_str.length());
     }
+    out=(power_sum==in_int);
+    if(isnum==false&&out==true){
+        isnum=true;
+    }  
+    return out;
 }
